05403dba7c1232588b9e7cc97c6781a1ce9315dd
Momirtum <emircpvpci12@gmail.com>
2025-04-27 16:29:26 +0300

    saved UNDO information for reset changes and discarded files

--- obj/Debug/net8.0/TradeWave.AssemblyInfo.cs
+++ obj/Debug/net8.0/TradeWave.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TradeWave")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5385072bf7eca0dab8913300b76b861dc53e876d")]
 [assembly: System.Reflection.AssemblyProductAttribute("TradeWave")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TradeWave")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
